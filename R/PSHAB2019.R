15ef4d1deae385e79d46b29547e081456f4d73dc
chelseahuei <145436548+chelseahuei@users.noreply.github.com>
2024-01-24 22:51:08 +0800

    Update PSHAB2019.R

--- R/PSHAB2019.R
+++ R/PSHAB2019.R
@@ -18,27 +18,27 @@
 #'
 #' @return A list will be return, including mag, Ftype, rRup, vs30, z25, lnY, sigma, phi, tau,
 #'                                          specT, period2, iflag, depth, disthypo, iRegion, mbInter,
-#'                                          mbSlab, ztor, CasBas, Z10.
+#'                                          mbSlab, pnwflag.
 #'
 #' @examples
 #' PSHAB2019(6, 20, 0, 0, 760, 1, 10, 7, 1)
 #' PSHAB2019(7, 10, 0, 0, 760, 1, 10, 7, 1)
 #'
 #' @export
-PSHAB2019 <- function(Mag, Rrup, Prd, ftype=0, Vs30, z25, depth, Rhypo, region=7, Ztor, Z1.0, CasBas=1) {
+PSHAB2019 <- function(Mag, Rrup, Prd, ftype=0, Vs30, z25, depth, Rhypo, region=7, pnwflag=1) {
   if (Prd != 0 & (Prd < 0.01 | Prd > 10)) {
     stop("Period out of range! \n\n")
   }
   #PSHAB2019 ( mag, evType, rRup, vs30, z25, ztor, region, lnY, sigma, phi, tau,
   #             rockPGA, specT, period2, iflag, ACadjfac, epiflag )
   retvals <- .Fortran("S35_PSHAB2019", mag=as.single(Mag), Ftype=as.single(ftype), rRup=as.single(Rrup),
-                      vs30=as.single(Vs30), z25=as.single(z25), ztor=as.single(Ztor), iRegion=as.integer(region),
+                      vs30=as.single(Vs30), z25=as.single(z25), iRegion=as.integer(region),
                       lnY=as.single(0.0), sigma=as.single(0.1), phi=as.single(0.0), tau=as.single(0.0), 
                       specT=as.single(Prd), period2=as.single(0), iflag=as.integer(1),
                       depth=as.single(depth), disthypo=as.single(Rhypo), mbInter=as.single(0), mbSlab=as.single(0),
-                      CasBas=as.single(CasBas), Z10=as.single(Z1.0))
+                      pnwflag=as.integer(pnwflag))
   names(retvals) <- c("mag", "Ftype", "rRup", "vs30", "z25", "lnY", "sigma", "phi", "tau",
                       "specT", "period2", "iflag", "depth", "disthypo", "iRegion", "mbInter", 
-                      "mbSlab", "ztor", "CasBas", "Z10")
+                      "mbSlab", "pnwflag")
   return(retvals)
 }

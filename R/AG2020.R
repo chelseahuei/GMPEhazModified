d267c1748c469b2bd6c89b6a025f4fd5ffe57066
chelseahuei <145436548+chelseahuei@users.noreply.github.com>
2024-01-24 13:24:15 +0800

    Update AG2020.R

--- R/AG2020.R
+++ R/AG2020.R
@@ -31,7 +31,7 @@ AG2020 <- function(Mag, Rrup, Prd, ftype=0, Vs30, z25, ztor, region=7) {
   #             rockPGA, specT, period2, iFlag, ACadjfac, epiflag )
   retvals <- .Fortran("AG2020", mag=as.single(Mag), evType=as.single(ftype), rRup=as.single(Rrup),
                       vs30=as.single(Vs30), z25=as.single(z25), ztor=as.single(ztor), region=as.single(region),
-                      mu=as.single(0), sigma=as.single(0), phi=as.single(0), tau=as.single(0), 
+                      mu=as.single(0.0), sigma=as.single(0.1), phi=as.single(0.0), tau=as.single(0.0), 
                       rockPGA=as.single(0), specT=as.single(Prd), period2=as.single(0), iflag=as.integer(1),
                       ACadjfac=as.single(0), epiflag=as.single(0))
   names(retvals) <- c("mag", "evType", "rRup", "vs30", "z25", "ztor", "region", "mu", "sigma", "phi", "tau",

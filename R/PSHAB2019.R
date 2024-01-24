211f478889d3b6a90da2ba4a6cc3e8088faf2682
chelseahuei <145436548+chelseahuei@users.noreply.github.com>
2024-01-24 23:58:40 +0800

    Update PSHAB2019.R

--- R/PSHAB2019.R
+++ R/PSHAB2019.R
@@ -17,7 +17,7 @@
 #' @param epiflag .
 #'
 #' @return A list will be return, including mag, Ftype, rRup, vs30, z25, lnY, sigma, phi, tau,
-#'                                          specT, period2, iflag, depth, disthypo, iRegion, mbInter,
+#'                                          specT, period2, iflag, depth, disthypo, region, mbInter,
 #'                                          mbSlab, pnwflag.
 #'
 #' @examples
@@ -35,10 +35,10 @@ PSHAB2019 <- function(Mag, Rrup, Prd, ftype=0, Vs30, z25, depth, Rhypo, region=7
                       vs30=as.single(Vs30), z25=as.single(z25),
                       lnY=as.single(0.0), sigma=as.single(0.1), phi=as.single(0.0), tau=as.single(0.0), 
                       specT=as.single(Prd), period2=as.single(0), iflag=as.integer(1),
-                      depth=as.single(depth), disthypo=as.single(Rhypo), iRegion=as.integer(region), mbInter=as.single(0), mbSlab=as.single(0),
+                      depth=as.single(depth), disthypo=as.single(Rhypo), region=as.integer(region), mbInter=as.single(0), mbSlab=as.single(0),
                       pnwflag=as.integer(pnwflag))
   names(retvals) <- c("mag", "Ftype", "rRup", "vs30", "z25", "lnY", "sigma", "phi", "tau",
-                      "specT", "period2", "iflag", "depth", "disthypo", "iRegion", "mbInter", 
+                      "specT", "period2", "iflag", "depth", "disthypo", "region", "mbInter", 
                       "mbSlab", "pnwflag")
   return(retvals)
 }

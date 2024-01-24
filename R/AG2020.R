767336ac24cbf54ec3b604da9e063b37d9c09136
chelseahuei <145436548+chelseahuei@users.noreply.github.com>
2024-01-24 21:58:21 +0800

    Update AG2020.R

--- R/AG2020.R
+++ R/AG2020.R
@@ -35,7 +35,7 @@ AG2020 <- function(Mag, Rrup, Prd, ftype=0, Vs30, z25, ztor, region=7, epiflag=1
                       mu=as.single(0.0), sigma=as.single(0.1), phi=as.single(0.0), tau=as.single(0.0), 
                       rockPGA=as.single(0), specT=as.single(Prd), period2=as.single(0), iflag=as.integer(1),
                       ACadjfac=as.single(0), epiflag=as.integer(epiflag))
-  names(retvals) <- c("mag", "evType", "rRup", "vs30", "z25", "ztor", "region", "mu", "sigma", "phi", "tau",
+  names(retvals) <- c("mag", "evType", "rRup", "vs30", "z25", "ztor", "region", "lnY", "sigma", "phi", "tau",
                       "rockPGA", "specT", "period2", "iflag", "ACadjfac", "epiflag")
   return(retvals)
 }

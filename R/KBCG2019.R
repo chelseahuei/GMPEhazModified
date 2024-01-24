5399a1f0af9aada122bce829b9af100ae39f5b8b
chelseahuei <145436548+chelseahuei@users.noreply.github.com>
2024-01-24 23:52:12 +0800

    Update KBCG2019.R

--- R/KBCG2019.R
+++ R/KBCG2019.R
@@ -25,7 +25,7 @@
 #' KBCG2019(7, 10, 0, 0, 760, 1, 10, 7, 1)
 #'
 #' @export
-KBCG2019 <- function(Mag, Rrup, Prd, ftype=0, Vs30, z25, depth, Rhypo, region=7, Ztor, Z1.0, CasBas=1) {
+KBCG2019 <- function(Mag, Rrup, Prd, ftype=0, Vs30, z25, depth, Rhypo, region=7, ztor, Z1.0, CasBas=1) {
   if (Prd != 0 & (Prd < 0.01 | Prd > 10)) {
     stop("Period out of range! \n\n")
   }
@@ -35,7 +35,7 @@ KBCG2019 <- function(Mag, Rrup, Prd, ftype=0, Vs30, z25, depth, Rhypo, region=7,
                       vs30=as.single(Vs30), z25=as.single(z25), lnY=as.single(0.0), sigma=as.single(0.1), phi=as.single(0.0), tau=as.single(0.0), 
                       specT=as.single(Prd), period2=as.single(0), iflag=as.integer(1),
                       depth=as.single(depth), disthypo=as.single(Rhypo), iRegion=as.integer(region), mbInter=as.single(0), mbSlab=as.single(0),
-                      ztor=as.single(Ztor), CasBas=as.integer(CasBas), Z10=as.single(Z1.0))
+                      ztor=as.single(ztor), CasBas=as.integer(CasBas), Z10=as.single(Z1.0))
   names(retvals) <- c("mag", "Ftype", "rRup", "vs30", "z25", "lnY", "sigma", "phi", "tau",
                       "specT", "period2", "iflag", "depth", "disthypo", "iRegion", "mbInter", 
                       "mbSlab", "ztor", "CasBas", "Z10")

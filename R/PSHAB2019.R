8872a629cee8952cac4e4166b6275e1a0bdaba83
chelseahuei <145436548+chelseahuei@users.noreply.github.com>
2024-01-24 23:23:57 +0800

    Update PSHAB2019.R

--- R/PSHAB2019.R
+++ R/PSHAB2019.R
@@ -32,10 +32,10 @@ PSHAB2019 <- function(Mag, Rrup, Prd, ftype=0, Vs30, z25, depth, Rhypo, region=7
   #PSHAB2019 ( mag, evType, rRup, vs30, z25, ztor, region, lnY, sigma, phi, tau,
   #             rockPGA, specT, period2, iflag, ACadjfac, epiflag )
   retvals <- .Fortran("S35_PSHAB2019", mag=as.single(Mag), Ftype=as.single(ftype), rRup=as.single(Rrup),
-                      vs30=as.single(Vs30), z25=as.single(z25), iRegion=as.integer(region),
+                      vs30=as.single(Vs30), z25=as.single(z25),
                       lnY=as.single(0.0), sigma=as.single(0.1), phi=as.single(0.0), tau=as.single(0.0), 
                       specT=as.single(Prd), period2=as.single(0), iflag=as.integer(1),
-                      depth=as.single(depth), disthypo=as.single(Rhypo), mbInter=as.single(0), mbSlab=as.single(0),
+                      depth=as.single(depth), disthypo=as.single(Rhypo), iRegion=as.integer(region), mbInter=as.single(0), mbSlab=as.single(0),
                       pnwflag=as.integer(pnwflag))
   names(retvals) <- c("mag", "Ftype", "rRup", "vs30", "z25", "lnY", "sigma", "phi", "tau",
                       "specT", "period2", "iflag", "depth", "disthypo", "iRegion", "mbInter", 

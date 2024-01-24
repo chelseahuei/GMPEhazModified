c30836776c507a0fea875ddb778858fb01d35704
chelseahuei <145436548+chelseahuei@users.noreply.github.com>
2024-01-24 23:21:56 +0800

    Update KBCG2019.R

--- R/KBCG2019.R
+++ R/KBCG2019.R
@@ -32,11 +32,10 @@ KBCG2019 <- function(Mag, Rrup, Prd, ftype=0, Vs30, z25, depth, Rhypo, region=7,
   #KBCG2019 ( mag, evType, rRup, vs30, z25, ztor, region, lnY, sigma, phi, tau,
   #             rockPGA, specT, period2, iflag, ACadjfac, epiflag )
   retvals <- .Fortran("S35_KBCG2019", mag=as.single(Mag), Ftype=as.single(ftype), rRup=as.single(Rrup),
-                      vs30=as.single(Vs30), z25=as.single(z25), ztor=as.single(Ztor), iRegion=as.integer(region),
-                      lnY=as.single(0.0), sigma=as.single(0.1), phi=as.single(0.0), tau=as.single(0.0), 
+                      vs30=as.single(Vs30), z25=as.single(z25), lnY=as.single(0.0), sigma=as.single(0.1), phi=as.single(0.0), tau=as.single(0.0), 
                       specT=as.single(Prd), period2=as.single(0), iflag=as.integer(1),
-                      depth=as.single(depth), disthypo=as.single(Rhypo), mbInter=as.single(0), mbSlab=as.single(0),
-                      CasBas=as.integer(CasBas), Z10=as.single(Z1.0))
+                      depth=as.single(depth), disthypo=as.single(Rhypo), iRegion=as.integer(region), mbInter=as.single(0), mbSlab=as.single(0),
+                      ztor=as.single(Ztor), CasBas=as.integer(CasBas), Z10=as.single(Z1.0))
   names(retvals) <- c("mag", "Ftype", "rRup", "vs30", "z25", "lnY", "sigma", "phi", "tau",
                       "specT", "period2", "iflag", "depth", "disthypo", "iRegion", "mbInter", 
                       "mbSlab", "ztor", "CasBas", "Z10")

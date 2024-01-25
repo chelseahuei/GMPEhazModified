9666a967add8d6a197211bb84aee188b4a59d8af
chelseahuei <145436548+chelseahuei@users.noreply.github.com>
2024-01-25 23:53:21 +0800

    Update KBCG2019.R

--- R/KBCG2019.R
+++ R/KBCG2019.R
@@ -34,7 +34,7 @@ KBCG2019 <- function(Mag, Rrup, Prd, ftype=0, Vs30, z25, depth, Rhypo, region=7,
   retvals <- .Fortran("S35_KBCG2019", mag=as.single(Mag), Ftype=as.single(ftype), rRup=as.single(Rrup),
                       vs30=as.single(Vs30), z25=as.single(z25), lnY=as.single(0.0), sigma=as.single(0.1), phi=as.single(0.0), tau=as.single(0.0), 
                       specT=as.single(Prd), period2=as.single(0), iflag=as.integer(1),
-                      depth=as.single(depth), disthypo=as.single(Rhypo), iRegion=as.integer(region), mbInter=as.single(0), mbSlab=as.single(0),
+                      depth=as.single(depth), disthypo=as.single(Rhypo), iRegion=as.integer(region), mbInter=as.single(7.1), mbSlab=as.single(7.7),
                       ztor=as.single(ztor), CasBas=as.integer(CasBas), Z10=as.single(Z1.0))
   names(retvals) <- c("mag", "Ftype", "rRup", "vs30", "z25", "lnY", "sigma", "phi", "tau",
                       "specT", "period2", "iflag", "depth", "disthypo", "iRegion", "mbInter", 
